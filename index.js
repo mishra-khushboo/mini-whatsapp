20a05a1790a626530ed45634a5fbd770ef4a9966
mishra-khushboo <khush27093@gmail.com>
2025-09-18 20:27:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("root is working");
+});
+
+app.listen(8080, () => {
+  console.log("server is running");
+});
+
+// const mongoose = require("mongoose");
+
+// main()
+//   .then(() => {
+//     console.log("success");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// async function main() {
+//   await mongoose.connect("mongodb://127.0.0.1:27017/test");
+// }
