8e00ca5337e54fa9fde4f515ee45dd4628a763b9
mishra-khushboo <khush27093@gmail.com>
2025-09-18 20:56:17 +0530

    1st lecture done D40

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+main()
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
 
 app.get("/", (req, res) => {
   res.send("root is working");
